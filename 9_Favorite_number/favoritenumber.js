1568a14a5e042b75a82b1e01e754f2f7a054e70d
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-28 21:18:24 +0500

    Add files via upload

---
+++ 9_Favorite_number/favoritenumber.js
@@ -0,0 +1,2 @@
+var favorite_Number = 8;
+console.log(" my favorite Number is ".concat(favorite_Number));
---
+++ 9_Favorite_number/favoritenumber.ts
@@ -0,0 +1,2 @@
+let favorite_Number: number = 8;
+console.log(` my favorite Number is ${favorite_Number}`);
