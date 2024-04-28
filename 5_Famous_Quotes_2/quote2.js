fcfd9a9ecd890c398d794bbba46003c192d7cb3e
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-28 17:07:53 +0500

    Add files via upload

---
+++ 5_Famous_Quotes_2/quote2.js
@@ -0,0 +1,4 @@
+var quote = "Fear God and you will have no cause to fear any one.";
+var famous_person = "Imam Ali";
+var message = "".concat(famous_person, " once said, \"").concat(quote, "\"");
+console.log(message);
---
+++ 5_Famous_Quotes_2/quote2.ts
@@ -0,0 +1,4 @@
+let quote : string = "Fear God and you will have no cause to fear any one.";
+let famous_person = "Imam Ali";
+let message = `${ famous_person } once said, "${quote}"`;
+console.log(message);
