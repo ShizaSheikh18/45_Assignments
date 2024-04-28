19036caaff3ce1b38da3ed3b9330a803216d744a
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-28 17:03:46 +0500

    Add files via upload

---
+++ 4_Famous_Quotes_1/famousquote.js
@@ -0,0 +1,3 @@
+var quote = "With faith, Discipline and Selfless devotion to duty, There is nothing worthwhile that you cannot achieve.";
+var author = "Mohammad Ali Jinnah ";
+console.log(" ".concat(author, " once said, \" ").concat(quote));
---
+++ 4_Famous_Quotes_1/famousquote.ts
@@ -0,0 +1,3 @@
+let quote : string = "With faith, Discipline and Selfless devotion to duty, There is nothing worthwhile that you cannot achieve.";
+let author : string = "Mohammad Ali Jinnah ";
+console.log(` ${ author } once said, " ${ quote }`);
