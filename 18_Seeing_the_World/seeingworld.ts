109ac336beee3a98581095e6f2b850ef05e5bebe
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 17:10:05 +0500

    Add files via upload

---
+++ 18_Seeing_the_World/seeingworld.ts
@@ -0,0 +1,21 @@
+let places :string[] = [' Saudia' , ' Germany', ' Karachi', ' Quetta', ' Sea view'];
+
+console.log('orignal ' + places);
+
+console.log('copy ' + [...places].sort());
+console.log(' original ' + places);
+
+console.log(' copy ' + [...places].sort().reverse());
+
+console.log('orignal' + places);
+
+console.log('original ' + places.reverse());
+
+console.log('original ' + places.reverse());
+
+console.log('orignal ' + places.sort());
+
+console.log('orignal ' + places.sort().reverse());
+
+
+
