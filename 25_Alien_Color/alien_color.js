0537953d7fa3cd58fafaa936c0c123051e9c0ba7
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 20:47:09 +0500

    Add files via upload

---
+++ 25_Alien_Color/alien_color.js
@@ -0,0 +1,9 @@
+//alien color
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Player just earned 5 points !");
+}
+alien_color = 'yellow';
+if (alien_color === 'green') {
+    console.log("Player just earned 6 points !");
+}
