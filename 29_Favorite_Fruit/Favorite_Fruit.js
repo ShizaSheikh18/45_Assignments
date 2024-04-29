8f33a29cb60644fc6c1bd5c16b08148458b5f297
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 21:09:17 +0500

    Add files via upload

---
+++ 29_Favorite_Fruit/Favorite_Fruit.js
@@ -0,0 +1,16 @@
+var Favorite_Fruit = ['Apple', 'Cherry', 'Mango'];
+if (Favorite_Fruit.includes('Cherry')) {
+    console.log('I really like Cherry !');
+}
+if (Favorite_Fruit.includes('Apple')) {
+    console.log('I really like Apple !');
+}
+if (Favorite_Fruit.includes('Mango')) {
+    console.log('I really like Mango');
+}
+if (Favorite_Fruit.includes('Strawberry')) {
+    console.log('I really like Strawberry');
+}
+if (Favorite_Fruit.includes('Orange')) {
+    console.log('I really like Orange');
+}
