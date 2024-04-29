0c703a240dae9a29894635421b8c9333f58eb076
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 17:03:06 +0500

    Add files via upload

---
+++ 14_Guest_List/guestlist.js
@@ -0,0 +1,4 @@
+var guest_list = [' Haider', ' Iqbal', ' Amna', ' Huma', ' Ayaan'];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam ' + guest_list[i] + ',\n Me invited you on Iftar Party tomorrow. \n\n Thank you');
+}
---
+++ 14_Guest_List/guestlist.ts
@@ -0,0 +1,4 @@
+let guest_list: string[] = [' Haider', ' Iqbal', ' Amna' ,' Huma', ' Ayaan'];
+for(let i = 0; i< guest_list.length; i++){
+    console.log('Respected Sir/Madam ' + guest_list[i] +',\n Me invited you on Iftar Party tomorrow. \n\n Thank you' );
+}
