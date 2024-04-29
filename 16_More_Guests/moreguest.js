457bdf9bc9cf67c08b0828ba52586a3c67f993c5
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 17:06:20 +0500

    Add files via upload

---
+++ 16_More_Guests/moreguest.js
@@ -0,0 +1,15 @@
+var guest_list = [' Haider', ' Iqbal', ' Amna', ' Huma', ' Ayaan'];
+// for(let i = 0; i< guest_list.length; i++){
+//     console.log('Respected Sir/Madam ' + guest_list[i] +',\n Me invited you on Iftar Party tomorrow. \n\n Thank you \n' );
+// }
+var not_present = " Ayaan ";
+var new_guest_list = ' Aqib';
+guest_list[4] = new_guest_list;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam ' + guest_list[i] + ',\n Me invited you on Iftar Party tomorrow. \n\n Thank you \n');
+}
+console.log("Mr.".concat(not_present, " will not coming tomorrow iftar party."));
+guest_list.unshift(' Aqsa', ' Hoorain ', ' Maryam');
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam ' + guest_list[i] + ',\n Me invited you on Iftar Party tomorrow. \n\n Thank you \n');
+}
