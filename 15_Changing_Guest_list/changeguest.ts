cc4a374d74e38dff6c269136e04053b1ed4153b9
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 17:04:32 +0500

    Add files via upload

---
+++ 15_Changing_Guest_list/changeguest.ts
@@ -0,0 +1,12 @@
+let guest_list: string[] = [' Haider', ' Iqbal', ' Amna' ,' Huma', ' Ayaan'];
+for(let i = 0; i< guest_list.length; i++){
+    console.log('Respected Sir/Madam ' + guest_list[i] +',\n Me invited you on Iftar Party tomorrow. \n\n Thank you \n' );
+}
+
+let not_present : string = " Ayaan ";
+let  new_guest_list : string = ' Aqib';
+guest_list[4] = new_guest_list;
+for(let i = 0; i< guest_list.length; i++){
+    console.log('Respected Sir/Madam ' + guest_list[i] +',\n Me invited you on Iftar Party tomorrow. \n\n Thank you \n' );
+}
+console.log(`Mr.${ not_present } will not coming tomorrow iftar party.`);
