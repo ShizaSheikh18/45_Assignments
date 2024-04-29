9865b16b9cb55966650ded7afcea66abd02d6918
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 17:30:27 +0500

    Add files via upload

---
+++ 21_Objects/objects.js
@@ -0,0 +1,16 @@
+var person = {
+    name: 'Shiza Shafi',
+    age: 22,
+    nationality: 'Pakistani',
+    city: 'Karachi',
+    student: true
+};
+console.log(person);
+var student_id = {
+    Name: 'Minal Shafi',
+    Father_name: 'M.Shafi',
+    Age: 18,
+    Class: 9,
+    Morning_shift: true,
+};
+console.log(student_id);
---
+++ 21_Objects/objects.ts
@@ -0,0 +1,34 @@
+interface person {
+    age : number,
+    name : string,
+    nationality : string,
+    city : string,
+    student : boolean,
+}
+
+let person :person = {
+    name : 'Shiza Shafi',
+    age : 22,
+    nationality : 'Pakistani',
+    city : 'Karachi',
+    student : true
+}
+console.log(person);
+
+interface student_id {
+    Name : string,
+    Father_name : string,
+    Age : number,
+    Class : number,
+    Morning_shift : boolean,
+}
+
+let student_id : student_id = {
+    Name : 'Minal Shafi',
+    Father_name : 'M.Shafi',
+    Age : 18,
+    Class : 9,
+    Morning_shift : true,
+}
+console.log(student_id);
+    
