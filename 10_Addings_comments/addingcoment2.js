53cf341851afd1ce30fe9d90ff0c0e36dd5c3799
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 16:56:11 +0500

    Add files via upload

---
+++ 10_Addings_comments/addingcoment2.js
@@ -0,0 +1,5 @@
+// Exercise :10
+// My name is Shiza
+// Date : 17-March-2024
+// This pogram will multiplication
+console.log(9 * 2);
---
+++ 10_Addings_comments/addingcomments.ts
@@ -0,0 +1,5 @@
+// Exercise :10
+// My name is Shiza
+// Date : 17-March-2024
+//this program will run simple code just like hello world
+console.log("hellow world simple program");
