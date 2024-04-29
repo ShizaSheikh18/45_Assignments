c639323387e1e8e889a8d862bb26de77143c2459
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 21:06:28 +0500

    Add files via upload

---
+++ 28_Stages_of _life/stages_of_life.js
@@ -0,0 +1,21 @@
+//Stages of life
+var age = 21;
+//Conditions 
+if (age < 2) {
+    console.log("Person is a Baby");
+}
+else if (age >= 2 && age < 4) {
+    console.log("Person is a Toddler ");
+}
+else if (age >= 4 && age < 13) {
+    console.log('Person is a Kid');
+}
+else if (age >= 13 && age < 20) {
+    console.log('Person is a Teenager');
+}
+else if (age >= 20 && age < 65) {
+    console.log('Person is an Adult');
+}
+else {
+    console.log("Person is an Elder");
+}
---
+++ 28_Stages_of _life/stages_of_life.ts
@@ -0,0 +1,20 @@
+//Stages of life
+ 
+let age : number = 21;
+
+//Conditions 
+
+if(age < 2){
+console.log("Person is a Baby");
+}else if(age >= 2 && age < 4 ){
+    console.log("Person is a Toddler ");
+}else if(age >= 4 && age < 13){
+    console.log('Person is a Kid');
+}else if(age >= 13 && age < 20){
+    console.log('Person is a Teenager');
+}else if(age >= 20 && age < 65){
+    console.log('Person is an Adult');
+}else{
+    console.log("Person is an Elder");
+}
+ 
