25d3410bee1fd39ceb63b35953f116b56df2db76
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 16:59:53 +0500

    Add files via upload

---
+++ 12_Greetings/greetings.js
@@ -0,0 +1,5 @@
+var personNames = ['Shiza', ' Sana', 'Alishba'];
+var message = "i have some Questions. Can i ask?";
+for (var i = 0; i < personNames.length; i++) {
+    console.log(message + personNames[i]);
+}
---
+++ 12_Greetings/greetings.ts
@@ -0,0 +1,5 @@
+let personNames : string[] = ['Shiza', ' Sana', 'Alishba'];
+let message : string = "i have some Questions. Can i ask?" ;
+for(let i = 0; i< personNames.length; i++){
+    console.log(message + personNames[i])
+}
