15d98a6c886227acf2cb556b0c76c51af3d58e9d
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 17:28:32 +0500

    Add files via upload

---
+++ 20_Empty_Array/emptyarray.js
@@ -0,0 +1,7 @@
+var cities = [' Maree', ' Sawat', ' Quetta'];
+// using .push 
+var countries = [];
+countries.push('Saudia');
+countries.push('Pakistan');
+countries.push('Germany');
+console.log(countries);
---
+++ 20_Empty_Array/emptyarray.ts
@@ -0,0 +1,9 @@
+let cities : string[] = [' Maree', ' Sawat', ' Quetta'];
+
+// using .push 
+
+let countries : string[] = [];
+countries.push('Saudia');
+countries.push('Pakistan');
+countries.push('Germany');
+console.log(countries)
