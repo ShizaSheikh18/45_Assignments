5f53b8b7f2633b445f7a28cb2b2bbbad9a8985e2
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 17:01:35 +0500

    Add files via upload

---
+++ 13_Own_Array/ownarray.ts
@@ -0,0 +1,4 @@
+let transportation: string[] = ['Car', 'Bike', 'Bus', 'Hiroof', 'Rickshaw'];
+for(let i = 0; i< transportation.length; i++){
+    console.log('i would like to own a ' + transportation[i]);
+}
