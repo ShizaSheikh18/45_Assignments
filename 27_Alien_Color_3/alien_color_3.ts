bc9a7b5aa86fe9b5718c4d4d0842c57eb2b9411b
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-04-29 21:01:10 +0500

    Add files via upload

---
+++ 27_Alien_Color_3/alien_color_3.ts
@@ -0,0 +1,50 @@
+//Alien color 
+let alienColor : string = "green";
+//version 1
+//Conditions
+if(alienColor === "green"){
+    console.log("Players earned 5 points.");
+
+}else if(alienColor === "yellow"){
+    console.log("Players earned 10 points.");
+
+}else if(alienColor === "red"){
+    console.log("Players earned 15 points.");
+
+}
+else{
+    console.log("Please select Right color");
+}
+//version 2
+alienColor = "yellow"
+
+if(alienColor === "green"){
+    console.log("Players earned 5 points.");
+
+}else if(alienColor === "yellow"){
+    console.log("Players earned 10 points.");
+
+}else if(alienColor === "red"){
+    console.log("Players earned 15 points.");
+
+}
+else{
+    console.log("Please select Right color");
+}
+
+//version 3
+alienColor = "red";
+
+if(alienColor === "green"){
+    console.log("Players earned 5 points.");
+
+}else if(alienColor === "yellow"){
+    console.log("Players earned 10 points.");
+
+}else if(alienColor === "red"){
+    console.log("Players earned 15 points.");
+
+}
+else{
+    console.log("Please select Right color");
+}
