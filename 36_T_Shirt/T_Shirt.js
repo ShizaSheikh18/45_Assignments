62b4270e51947c660d48512a7aa72a69f91f92c9
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-05-02 00:48:36 +0500

    Add files via upload

---
+++ 36_T_Shirt/T_Shirt.js
@@ -0,0 +1,6 @@
+function make_shirt(size, text) {
+    console.log("Creating a ".concat(size, " shirt with the message: ").concat(text));
+}
+make_shirt('Large', 'HELLO WORLD');
+make_shirt('Medium', 'HELLO WORLD');
+make_shirt('Small', 'HELLO WORLD');
---
+++ 36_T_Shirt/T_Shirt.ts
@@ -0,0 +1,7 @@
+function make_shirt(size:string, text:string){
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+
+make_shirt('Large', 'HELLO WORLD');
+make_shirt('Medium', 'HELLO WORLD');
+make_shirt('Small', 'HELLO WORLD');
