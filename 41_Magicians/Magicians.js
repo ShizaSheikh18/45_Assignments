cac837ecad0dd1ba818b90e290a0581a7f332cf7
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-05-02 00:58:08 +0500

    Add files via upload

---
+++ 41_Magicians/Magicians.js
@@ -0,0 +1,7 @@
+var Magician = ['Harry Potter', 'Hermione Granger', 'Ron Weasley', 'Albus Dumbledore'];
+function show_magicians(Magicians) {
+    Magician.forEach(function (element) {
+        console.log(element);
+    });
+}
+show_magicians(Magician);
---
+++ 41_Magicians/Magicians.ts
@@ -0,0 +1,9 @@
+let Magician: string[] = ['Harry Potter', 'Hermione Granger', 'Ron Weasley', 'Albus Dumbledore'];
+
+function show_magicians(Magicians : string[]){
+     Magician.forEach( element => {
+    console.log(element);
+     });
+}
+
+show_magicians(Magician);
