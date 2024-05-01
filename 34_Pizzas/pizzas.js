98fddc6980eebf08839e2bc679fe9660fc1a7ef3
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-05-02 00:44:01 +0500

    Add files via upload

---
+++ 34_Pizzas/pizzas.js
@@ -0,0 +1,6 @@
+"use strict";
+let pizzas_name = ['Chicken Tikka Pizza', 'Chicken Fajita Pizza', ' BBQ Chicken Pizza'];
+for (let i = 0; i < pizzas_name.length; i++) {
+    console.log(`I like ${pizzas_name[i]}`);
+}
+console.log("I really like pizza ");
---
+++ 34_Pizzas/pizzas.ts
@@ -0,0 +1,6 @@
+let pizzas_name : string [] = ['Chicken Tikka Pizza', 'Chicken Fajita Pizza', ' BBQ Chicken Pizza'];
+
+for(let i = 0; i< pizzas_name.length; i++){
+console.log(`I like ${pizzas_name[i]}`);
+}
+console.log("I really like pizza ");
