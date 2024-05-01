d9ea9d8142cf4d0aa8ab442e1c606469d61ef0c4
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-05-02 00:54:33 +0500

    Add files via upload

---
+++ 39_City_Name/City_Name.js
@@ -0,0 +1,7 @@
+"use strict";
+function City_country(city, country) {
+    return `${city} , ${country}`;
+}
+console.log(City_country('Karachi', 'Pakistan'));
+console.log(City_country('Tokyo', 'Japan'));
+console.log(City_country('Paris', 'France'));
---
+++ 39_City_Name/City_Name.ts
@@ -0,0 +1,9 @@
+function City_country(city: string, country: string){
+    return `${city} , ${country}`  
+}
+
+console.log (City_country ('Karachi', 'Pakistan'));
+console.log (City_country ('Tokyo', 'Japan'));
+console.log (City_country ('Paris', 'France'));
+
+
