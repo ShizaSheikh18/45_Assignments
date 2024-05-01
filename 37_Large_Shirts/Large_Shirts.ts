90a31fad7d66ced93cc4604d26a64ed96b878195
ShizaSheikh18 <161851114+ShizaSheikh18@users.noreply.github.com>
2024-05-02 00:50:26 +0500

    Add files via upload

---
+++ 37_Large_Shirts/Large_Shirts.ts
@@ -0,0 +1,6 @@
+function make_shirt(size:string = 'Large', text:string = 'I love Typescript'){
+   console.log(`Creating a ${size} shirt with message: ${text}`);
+}
+make_shirt();
+make_shirt('Medium');
+make_shirt('Small', 'I love Pakistan');
